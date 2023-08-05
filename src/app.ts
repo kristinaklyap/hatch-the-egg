a4c0201f8a62e3f9ecaecee8af99aa59a160839e
kristinkaklyap <kristinka.klyap@gmail.com>
2023-08-05 14:07:19 +0200

    implement game init

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("śmiga");
-});
+import { Game } from './modules/game.js'
+
+document.addEventListener('DOMContentLoaded', function () {
+    const counter = document.querySelector('#counter') as HTMLParagraphElement
+    const egg = document.querySelector('#egg') as HTMLImageElement
+    const game = new Game()
+    game.init({ counterElement: counter, eggElement: egg })
+})
