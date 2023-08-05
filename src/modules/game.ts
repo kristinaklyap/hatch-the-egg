04418713a90a81a670fc61c00b35d13aa8e7d829
kristinkaklyap <kristinka.klyap@gmail.com>
2023-08-05 15:23:32 +0200

    implement hatching egg

--- src/modules/game.ts
+++ src/modules/game.ts
@@ -1,62 +1,106 @@
-// game.ts
 import { Egg, EggState } from './egg.js'
+
 interface GameParams {
-    eggElement: HTMLImageElement
-    counterElement: HTMLParagraphElement
+    eggElement: HTMLImageElement | null
+    counterElement: HTMLParagraphElement | null
+    resultElement: HTMLParagraphElement | null
 }
 
 interface IGame extends GameParams {}
 
-export class Game {
+export class Game implements IGame {
     counterElement: HTMLParagraphElement | null = null
+    resultElement: HTMLParagraphElement | null = null
     eggElement: HTMLImageElement | null = null
-    eggInstance: Egg = new Egg()
     stopWatch: number | null = null
     secondsPassed: number = 0
+    eggInstance: Egg = new Egg({
+        clicksToHatch: 30,
+        onEggHatch: this.hatchEgg.bind(this),
+    })
 
     init(params: GameParams) {
         if (!params.counterElement || !params.eggElement) {
-            //union type
             throw new Error('One of elements not found')
         }
-
         this.counterElement = params.counterElement
         this.eggElement = params.eggElement
+        this.resultElement = params.resultElement
         this.displayEggClicks()
         this.mountEgg()
+        console.log('Game started')
     }
 
     displayEggClicks() {
         if (!this.counterElement) {
-            throw new Error('One of elements not found')
-        }
-        this.counterElement.innerText = `${this.eggInstance.eggClicks}`
-    }
-
-    mountEgg() {
-        if (!this.eggElement) {
-            throw new Error('Egg of elements not found')
+            throw new Error('Counter element not found')
         }
 
-        const eggImageSrc = this.eggInstance.assets.get(EggState.Egg)
-        if (!eggImageSrc) {
-            throw new Error('no image was not found')
-        }
-
-        this.eggElement.src = eggImageSrc
-        this.eggElement.addEventListener('click', () => {
-            this.eggInstance.tapEgg()
-            this.displayEggClicks()
-            if (!this.stopWatch) {
-                this.startStopWatch()
-            }
-        })
+        this.counterElement.innerText = String(this.eggInstance.eggClicks)
     }
 
     startStopWatch() {
         this.stopWatch = setInterval(() => {
-            this.secondsPassed++
-            console.log('stopwatch started', this.secondsPassed)
-        }, 1000)
+            this.secondsPassed = this.secondsPassed + 100
+        }, 100)
+    }
+
+    stopStopWatch() {
+        if (!this.stopWatch) {
+            throw new Error('Stop watch not found')
+        }
+
+        clearInterval(this.stopWatch)
+    }
+
+    updateEggClick() {
+        this.eggInstance.tapEgg()
+        this.displayEggClicks()
+
+        switch (this.eggInstance.eggClicks) {
+            case 1:
+                this.startStopWatch()
+                break
+        }
+    }
+
+    mountEgg() {
+        if (!this.eggElement) {
+            throw new Error('Egg element not found')
+        }
+
+        const eggImageSrc = this.eggInstance.assets.get(EggState.Egg)
+
+        if (!eggImageSrc) {
+            throw new Error('Egg image src not found')
+        }
+
+        this.eggElement.src = eggImageSrc
+        this.eggElement.addEventListener(
+            'click',
+            this.updateEggClick.bind(this)
+        )
+    }
+
+    hatchEgg() {
+        if (!this.eggElement) {
+            throw new Error('Egg element not found')
+        }
+
+        const eggImageSrc = this.eggInstance.assets.get(EggState.Tamagotchi)
+
+        if (!eggImageSrc) {
+            throw new Error('Egg image src not found')
+        }
+
+        if (!this.resultElement) {
+            throw new Error('Result element not found')
+        }
+
+        this.eggElement.src = eggImageSrc
+        this.resultElement.innerText =
+            (this.secondsPassed / 1000).toString() + ' seconds'
+
+        this.stopStopWatch()
     }
 }
